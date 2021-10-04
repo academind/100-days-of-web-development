85b6417a690fa078f52aa1bc2f69a7bb2cfe8c9c
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-10-04 16:50:34 +0200

    adjusted code

--- code/04-creating-our-first-model/routes/auth.js
+++ code/04-creating-our-first-model/routes/auth.js
@@ -53,7 +53,7 @@ router.post('/signup', async function (req, res) {
     !enteredEmail ||
     !enteredConfirmEmail ||
     !enteredPassword ||
-    enteredPassword.trim() < 6 ||
+    enteredPassword.trim().length < 6 ||
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes('@')
   ) {
