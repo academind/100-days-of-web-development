24226ce431ce9863b3d931898f28dc22bfe2961f
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-11-09 10:21:20 +0100

    replaced localhost with 127.0.0.1

--- code/00-starting-projects/xss/data/database.js
+++ code/00-starting-projects/xss/data/database.js
@@ -6,7 +6,7 @@ let database;
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(
-    'mongodb://localhost:27017'
+    'mongodb://127.0.0.1:27017'
   );
   database = client.db('security');
 }
