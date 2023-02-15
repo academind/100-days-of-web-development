785ed2f2facd8cf928d119cd54b8a0bd5e323bf3
echi0517 <104536231+echi0517@users.noreply.github.com>
2023-02-16 05:29:45 +0900

    Add files via upload
    ㅇㅇ

---
+++ 06_js/app.js
@@ -0,0 +1,12 @@
+let age = 32;
+let userName = `Max`;
+let hobbies = ['축구','농구','배구'];
+
+let job = { 
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+
+let adultYears = age - 18
+alert(adultYears)
